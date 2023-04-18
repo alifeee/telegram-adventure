5bcda58df1628a444f92f0f588d3c75e205f787f
alifeee <alifeeerenn@hotmail.com>
2023-04-18 03:36:08 +0100

    Make bot work

--- src/bot.py
+++ src/bot.py
@@ -7,6 +7,8 @@ from telegram.ext import (
     ContextTypes,
     MessageHandler,
     filters,
+    ConversationHandler,
+    filters,
 )
 from telegram import (
     Update,
@@ -14,6 +16,7 @@ from telegram import (
     ReplyKeyboardMarkup,
     ReplyKeyboardRemove,
 )
+from database import Database
 
 
 load_dotenv()
@@ -36,10 +39,93 @@ async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     )
 
 
+# adventure step returns function
+def adventure_step(
+        node_id: str,
+        ends: list,
+        description: str,
+        responses: list
+):
+    buttons = [
+        [
+            KeyboardButton(response)
+        ] for response in responses
+    ]
+
+    async def adventure(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        await update.effective_message.reply_text(
+            description,
+            reply_markup=ReplyKeyboardMarkup(
+                buttons, one_time_keyboard=True, resize_keyboard=True, is_persistent=True),
+        )
+        if node_id in ends:
+            await update.effective_message.reply_text(
+                "You have reached the end of the adventure. Use /start to begin again.",
+                reply_markup=ReplyKeyboardRemove(),
+            )
+            return ConversationHandler.END
+        return node_id
+
+    return adventure
+
+
 def main() -> None:
     application = Application.builder().token(API_KEY).build()
 
     application.add_handler(CommandHandler("help", help))
+
+    db = Database("adventure.db")
+    node_ids = db.get_all_node_ids()
+    descriptions = {
+        node_id: db.get_node(node_id)["description"]
+        for node_id in node_ids
+    }
+    responses = {
+        node_id: [
+            response["properties"]["choice"]
+            for response in db.get_choices_for_node(node_id)
+        ]
+        for node_id in node_ids
+    }
+    response_locations = {
+        node_id: [
+            response["target"]
+            for response in db.get_choices_for_node(node_id)
+        ]
+        for node_id in node_ids
+    }
+
+    START = "forest hut"
+    ENDS = ["forest exit"]
+    entry_points = [
+        CommandHandler("start", adventure_step(
+            START,
+            ENDS,
+            descriptions[START],
+            responses[START]
+        ))
+    ]
+    states = {
+        node_id: [
+            MessageHandler(
+                filters.Regex(response),
+                adventure_step(
+                    location,
+                    ENDS,
+                    descriptions[location],
+                    responses[location]
+                )
+            )
+            for response, location in zip(responses[node_id], response_locations[node_id])
+        ]
+        for node_id in node_ids
+    }
+
+    application.add_handler(ConversationHandler(
+        entry_points=entry_points,
+        states=states,
+        fallbacks=[],
+    ))
 
     application.run_polling()
 
